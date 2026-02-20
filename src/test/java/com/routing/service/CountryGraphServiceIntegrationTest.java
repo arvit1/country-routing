8cac4b50f154d0b3e8a1e93a3d604e1680f22c90
arvit1 <avarfaj86@yahoo.com>
2026-02-20 23:34:11 +0100

    Refactor init method for country graph loading
    Refactor init method to remove virtual thread usage and simplify error handling.

--- src/test/java/com/routing/service/CountryGraphServiceIntegrationTest.java
+++ src/test/java/com/routing/service/CountryGraphServiceIntegrationTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class CountryGraphServiceIT {
+class CountryGraphServiceIntegrationTest {
 
     @Autowired
     CountryGraphService countryGraphService;
